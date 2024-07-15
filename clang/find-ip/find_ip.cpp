e6d010b5a1ab71fdfed287e9211a09487b07c066
wholetone games <59148965+wholetonegames@users.noreply.github.com>
2024-07-15 10:14:16 +0100

    works

--- clang/find-ip/find_ip.cpp
+++ clang/find-ip/find_ip.cpp
@@ -1,50 +1,34 @@
+#include <windows.h>
+#include <wininet.h>
+#include <string>
 #include <iostream>
-#include <winsock2.h>
-#include <ws2tcpip.h>
 
-#pragma comment(lib, "ws2_32.lib")
+std::string real_ip()
+{
+
+    HINTERNET net = InternetOpen("IP retriever",
+                                 INTERNET_OPEN_TYPE_PRECONFIG,
+                                 NULL,
+                                 NULL,
+                                 0);
+
+    HINTERNET conn = InternetOpenUrl(net,
+                                     "http://myexternalip.com/raw",
+                                     NULL,
+                                     0,
+                                     INTERNET_FLAG_RELOAD,
+                                     0);
+
+    char buffer[4096];
+    DWORD read;
+
+    InternetReadFile(conn, buffer, sizeof(buffer) / sizeof(buffer[0]), &read);
+    InternetCloseHandle(net);
+
+    return std::string(buffer, read);
+}
 
 int main()
 {
-    // Initialize Winsock
-    WSADATA wsaData;
-    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (result != 0)
-    {
-        std::cerr << "WSAStartup failed: " << result << std::endl;
-        return 1;
-    }
-
-    // Get the local hostname
-    char hostname[256];
-    result = gethostname(hostname, sizeof(hostname));
-    if (result == SOCKET_ERROR)
-    {
-        std::cerr << "gethostname failed: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 1;
-    }
-
-    // Get the host information
-    struct hostent *host = gethostbyname(hostname);
-    if (host == nullptr)
-    {
-        std::cerr << "gethostbyname failed: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 1;
-    }
-
-    // Print the IP addresses
-    for (int i = 0; host->h_addr_list[i] != nullptr; i++)
-    {
-        std::cout << "i: " << i << std::endl;
-        struct in_addr addr;
-        memcpy(&addr, host->h_addr_list[i], sizeof(struct in_addr));
-        std::cout << "IP Address: " << inet_ntoa(addr) << std::endl;
-    }
-
-    // Cleanup
-    WSACleanup();
-
-    return 0;
+    std::cout << real_ip() << "\n";
 }
--- clang/find-ip/run.bat
+++ clang/find-ip/run.bat
@@ -1,5 +1,6 @@
 C:\MinGW\bin\g++ find_ip.cpp^
  -lws2_32^
+ -lwininet^
  -lmingw32^
  -w -Wl,-subsystem,console^
  -o myip
