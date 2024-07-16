2c39bd6bc7e5e2e03a16d274a3c231f8647232ef
wholetone games <59148965+wholetonegames@users.noreply.github.com>
2024-07-16 08:45:00 +0100

    resize

---
+++ clang/sfml-resize/run.bat
@@ -0,0 +1,8 @@
+g++ *.cpp^
+ -IC:\SFML-2.5.1\include^
+ -LC:\SFML-2.5.1\lib^
+ -lmingw32^
+ -l sfml-system^
+ -l sfml-window^
+ -l sfml-graphics^
+ -o ./output/game
---
+++ clang/sfml-resize/test.cpp
@@ -0,0 +1,36 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    // create own view
+    sf::View view = window.getDefaultView();
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (event.type == sf::Event::Resized)
+            {
+                // resize my view
+                view.setSize({static_cast<float>(event.size.width),
+                              static_cast<float>(event.size.height)});
+                window.setView(view); // and align shape
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
